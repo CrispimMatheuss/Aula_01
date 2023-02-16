c50842e396aadbf6ac4909e57caf811ceca44826
CrispimMatheuss <crispimmatheus@gmail.com>
2023-02-15 21:33:31 -0300

    a

--- src/TiposDeVariaveis.java
+++ src/TiposDeVariaveis.java
@@ -9,10 +9,25 @@ public class TiposDeVariaveis {
         char gabarito = 'A';
         System.out.println("Questão selecionada: " + gabarito);
 
-        byte variavel = 27;
+        byte variavel = 127;
         System.out.println("Valor do byte: " + variavel);
 
         short numero = 32767;
         System.out.println("Valor do número: " + numero);
+
+        int numeroInteiro = 2147463647;
+        System.out.println("O valor máximo para um inteiro: " + numeroInteiro);
+
+        long numeroLongo = 9223372036854775807L;
+        System.out.println("O valor máximo para um longo é: " + numeroLongo);
+
+        float numeroPontoFlutuante = 3.40282347123214F;
+        System.out.println(numeroPontoFlutuante);
+
+        double numeroDouble = 3.48282345565656565;
+        System.out.println(numeroDouble);
+
+
+
     }
 }
