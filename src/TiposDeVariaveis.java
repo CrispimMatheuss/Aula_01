11ff7b8d9538864d04767dd2ff8572ef379cd2c6
CrispimMatheuss <crispimmatheus@gmail.com>
2023-02-15 21:21:58 -0300

    a

---
+++ src/TiposDeVariaveis.java
@@ -0,0 +1,18 @@
+public class TiposDeVariaveis {
+    public static void main(String[] args) {
+        boolean isTradicional = true;
+        System.out.println("É tradicional: "+ isTradicional);
+
+        boolean isConvencional = false;
+        System.out.println("É convencional: " + isConvencional);
+
+        char gabarito = 'A';
+        System.out.println("Questão selecionada: " + gabarito);
+
+        byte variavel = 27;
+        System.out.println("Valor do byte: " + variavel);
+
+        short numero = 32767;
+        System.out.println("Valor do número: " + numero);
+    }
+}
